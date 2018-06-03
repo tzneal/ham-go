e1753214aaaf7d494bc7550e7d7d93794d627a14
Todd Neal <todd@tneal.org>
2018-06-03 11:44:18 -0500

    fix deletion of bookmarks

--- bookmarks.go
+++ bookmarks.go
@@ -58,8 +58,9 @@ func (b *Bookmarks) AddBookmark(m Bookmark) {
 	b.Bookmark = append(b.Bookmark, m)
 }
 
+// RemoveAt removes an existing bookmark
 func (b *Bookmarks) RemoveAt(idx int) {
-	if idx <= 0 || idx >= len(b.Bookmark) {
+	if idx < 0 || idx >= len(b.Bookmark) {
 		return
 	}
 	b.Bookmark = append(b.Bookmark[:idx], b.Bookmark[idx+1:]...)
