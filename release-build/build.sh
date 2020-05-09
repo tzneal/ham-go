abe3a547da89d6bc5dd1012fa8bc55db55a869e2
Todd Neal <todd@tneal.org>
2020-05-09 06:57:55 -0500

    update release build scripts

--- release-build/build.sh
+++ release-build/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-docker build --no-cache -f Dockerfile -t termlog-build .
+if [ $# -ne 1 ]; then
+  echo 1>&2 "Usage: $0 tagname"
+  exit 3
+fi
+docker build -f Dockerfile --build-arg TAG=$1 -t termlog-build .
 for arch in amd64 arm arm64; do
-  docker run --rm -v `pwd`:/tmp/dist termlog-build-arm64:latest cp /go/src/github.com/tzneal/ham-go/cmd/termlog/termlog.${arch} /tmp/dist/
+  docker run --rm -v `pwd`:/tmp/dist termlog-build:latest cp /go/src/github.com/tzneal/ham-go/cmd/termlog/termlog.${arch} /tmp/dist/
 done
