b4c38acb7c0076b3ddfbb2e84caeae4e3850be61
Todd Neal <todd@tneal.org>
2020-05-12 20:10:05 -0500

    detect and support network connections for hamlib rigctld support

--- cmd/termlog/newrig.go
+++ cmd/termlog/newrig.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"strconv"
+	"strings"
 
 	"github.com/dh1tw/goHamlib"
 )
@@ -31,11 +34,35 @@ func newRig(cfg Rig) (*goHamlib.Rig, error) {
 	p.Stopbits = cfg.StopBits
 	p.Parity = goHamlib.ParityNone // TODO: make configurable
 	p.Handshake = goHamlib.HandshakeNone
-	p.RigPortType = goHamlib.RigPortSerial
+	if isHostPort(cfg.Port) {
+		p.RigPortType = goHamlib.RigPortNetwork
+	} else {
+		p.RigPortType = goHamlib.RigPortSerial
+	}
 	rig.SetPort(p)
 	// and open the rig
 	if err := rig.Open(); err != nil {
 		return nil, err
 	}
 	return rig, nil
+}
+
+func isHostPort(port string) bool {
+	// must be of the form host:port
+	segs := strings.Split(port, ":")
+	if len(segs) != 2 {
+		return false
+	}
+
+	// is the last part a port number?
+	_, err := strconv.Atoi(segs[1])
+	if err != nil {
+		return false
+	}
+	// can we resolve the first part?
+	_, err = net.ResolveIPAddr("ip", segs[0])
+	if err != nil {
+		return false
+	}
+	return true
 }
